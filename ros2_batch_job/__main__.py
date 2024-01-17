a98d3b00e69d6bfe5d24641e3328f291b36e30c7
Restyled.io <commits@restyled.io>
2024-01-17 17:39:37 +0000

    Restyled by pyment

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -120,6 +120,11 @@ colcon_space_defaults = {
 
 
 def main(sysargv=None):
+    """
+
+    :param sysargv:  (Default value = None)
+
+    """
     args = get_args(sysargv=sysargv)
     blacklisted_package_names = []
     if not args.packaging:
@@ -168,6 +173,11 @@ def main(sysargv=None):
 
 
 def get_args(sysargv=None):
+    """
+
+    :param sysargv:  (Default value = None)
+
+    """
     parser = argparse.ArgumentParser(
         description="Builds the ROS2 repositories as a single batch job"
     )
@@ -326,6 +336,12 @@ def get_args(sysargv=None):
 
 
 def process_coverage(args, job):
+    """
+
+    :param args: 
+    :param job: 
+
+    """
     print("# BEGIN SUBSECTION: coverage analysis")
     # Capture all gdca/gcno files (all them inside buildspace)
     coverage_file = os.path.join(args.buildspace, "coverage.info")
@@ -366,6 +382,12 @@ def process_coverage(args, job):
 
 
 def build_and_test(args, job):
+    """
+
+    :param args: 
+    :param job: 
+
+    """
     compile_with_clang = args.compile_with_clang and args.os == "linux"
 
     print("# BEGIN SUBSECTION: build")
@@ -500,6 +522,13 @@ def build_and_test(args, job):
 
 
 def run(args, build_function, blacklisted_package_names=None):
+    """
+
+    :param args: 
+    :param build_function: 
+    :param blacklisted_package_names:  (Default value = None)
+
+    """
     if blacklisted_package_names is None:
         blacklisted_package_names = []
     if args.force_ansi_color:
@@ -609,6 +638,11 @@ def run(args, build_function, blacklisted_package_names=None):
     with change_directory(args.workspace):
 
         def need_package_from_pipy(pkg_name):
+            """
+
+            :param pkg_name: 
+
+            """
             try:
                 importlib.import_module(pkg_name)
             except ModuleNotFoundError:
@@ -994,7 +1028,13 @@ def run(args, build_function, blacklisted_package_names=None):
 
 
 def _fetch_repos_file(url, filename, job):
-    """Use curl to fetch a repos file and display the contents."""
+    """Use curl to fetch a repos file and display the contents.
+
+    :param url: 
+    :param filename: 
+    :param job: 
+
+    """
 
     job.run(["curl", "-skL", url, "-o", filename])
     log("@{bf}==>@| Contents of `%s`:" % filename)
